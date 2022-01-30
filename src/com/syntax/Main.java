fc342fa5da692a312fbff01e07f0e45426aab6ab
marinap <marishkapuzikova@gmail.com>
2022-01-30 01:36:54 -0500

    the planet2 features has been added

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -13,6 +13,9 @@ public class Main {
         solana.Heat="6578897098";
         solana.radius="684979";
         solana.temp="1234567c";
-
+//adding featuures planet2
+        Planet2 perry=new Planet2();
+        perry.color="yellow";
+        perry.size="1234322456";
     }
 }
