e7445d6aba5f737f5e7e2bd3ad77a8eb7e015a0d
marinap <marishkapuzikova@gmail.com>
2022-01-29 23:34:22 -0500

    the issues in feature Sun has been resolved

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -12,7 +12,7 @@ public class Main {
         Sun solana=new Sun();
         solana.Heat="6578897098";
         solana.radius="684979";
-        solana.temp=1234567c;
+        solana.temp="1234567c";
 
     }
 }
